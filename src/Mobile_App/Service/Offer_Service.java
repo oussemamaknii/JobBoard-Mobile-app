53aeedf86472126ef0d6511cac6633870cbefe34
oussema makni <oussema.makni@esprit.tn>
2021-04-30 15:26:57 +0100

    update

--- src/Mobile_App/Service/Offer_Service.java
+++ src/Mobile_App/Service/Offer_Service.java
@@ -49,39 +49,11 @@ public class Offer_Service {
     public ArrayList<Offre_Emploi> parseTasks(String jsonText) {
         try {
             Offers = new ArrayList<>();
-            JSONParser j = new JSONParser();// Instanciation d'un objet JSONParser permettant le parsing du résultat json
-
-            /*
-                On doit convertir notre réponse texte en CharArray à fin de
-            permettre au JSONParser de la lire et la manipuler d'ou vient
-            l'utilité de new CharArrayReader(json.toCharArray())
-
-            La méthode parse json retourne une MAP<String,Object> ou String est
-            la clé principale de notre résultat.
-            Dans notre cas la clé principale n'est pas définie cela ne veux pas
-            dire qu'elle est manquante mais plutôt gardée à la valeur par defaut
-            qui est root.
-            En fait c'est la clé de l'objet qui englobe la totalité des objets
-                    c'est la clé définissant le tableau de tâches.
-            */
+            JSONParser j = new JSONParser();
             Map<String, Object> tasksListJson = j.parseJSON(new CharArrayReader(jsonText.toCharArray()));
-
-              /* Ici on récupère l'objet contenant notre liste dans une liste
-            d'objets json List<MAP<String,Object>> ou chaque Map est une tâche.
-
-            Le format Json impose que l'objet soit définit sous forme
-            de clé valeur avec la valeur elle même peut être un objet Json.
-            Pour cela on utilise la structure Map comme elle est la structure la
-            plus adéquate en Java pour stocker des couples Key/Value.
-
-            Pour le cas d'un tableau (Json Array) contenant plusieurs objets
-            sa valeur est une liste d'objets Json, donc une liste de Map
-            */
+            System.out.println(jsonText);
             List<Map<String, Object>> list = (List<Map<String, Object>>) tasksListJson.get("root");
-
-            //Parcourir la liste des tâches Json
             for (Map<String, Object> obj : list) {
-                //Création des tâches et récupération de leurs données
                 Offre_Emploi t = new Offre_Emploi();
                 float id = Float.parseFloat(obj.get("id").toString());
                 t.setId((int) id);
@@ -97,16 +69,8 @@ public class Offer_Service {
                 t.setCategory_id(Integer.parseInt(obj.get("categorie_id").toString()));
                 Offers.add(t);
             }
-
-
         } catch (IOException ex) {
-
         }
-         /*
-            A ce niveau on a pu récupérer une liste des tâches à partir
-        de la base de données à travers un service web
-
-        */
         return Offers;
     }
 
