244da14e7dbf63bf65c5d920fb1997580a028dc2
Ryaan <merghed.rayen@esprit.tn>
2021-05-17 18:22:27 +0100

    finally company crud working

--- src/Mobile_App/Gui/User/AddEditCompany.java
+++ src/Mobile_App/Gui/User/AddEditCompany.java
@@ -11,76 +11,43 @@ import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.ui.spinner.Picker;
 import com.codename1.ui.util.Resources;
 
-
 public class AddEditCompany extends SideMenu {
     public AddEditCompany(Form previous, company comp, Resources res) {
-        setTitle("Update Company");
+        setTitle("Update your Company");
         setLayout(BoxLayout.y());
-        if (comp == null) {
-            System.out.println("hi");
-            //  resume.getUser_id() != Session.ConnectedUser.getId()
-            TextField tfCompanyName = new TextField("", "Ex: Jobhub");
-            //   assert comp != null;
-            TextField tfContactEmail = new TextField("", "Ex: jobhub@business.tn");
-            TextField tfWebsite = new TextField("", "Ex: www.jobhub.tn");
-            Picker date = new Picker();
-            TextField tfCategory = new TextField("", "Ex: Software Developpment company");
-            TextField tfDescription = new TextField("", "Ex: ");
-            TextField tfCompanyAdress = new TextField("", "Ex: Tunisia, Gafsa");
-            TextField tfContactPhone = new TextField("", "Ex: +21620202020");
-            TextField tfFacebookLink = new TextField("", "Ex: www.facebook.com/jobhub");
-            Button companyButton = new Button("Add Company");
-            companyButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent evt) {
-                    try {
-                        company comp = new company(0, Session.ConnectedUser.getId(), 1, Integer.parseInt(tfContactPhone.getText()), tfWebsite.getText(), tfCategory.getText(),
-                                tfDescription.getText(), tfCompanyName.getText(), tfContactEmail.getText(), tfCompanyAdress.getText(), tfFacebookLink.getText(), date.getDate());
-                        if (addEditCompanyService.getInstance().addEditCompany(comp)) {
-                            Dialog.show("Success", "Added Successfully !", new Command("OK"));
-                        } else
-                            Dialog.show("ERROR", "Server error", new Command("OK"));
-                    } catch (NumberFormatException e) {
-                        Dialog.show("ERROR", "Status must be a number", new Command("OK"));
-                    }
+
+        System.out.println("hi");
+        //  resume.getUser_id() != Session.ConnectedUser.getId()
+        TextField tfCompanyName = new TextField("", "Ex: Jobhub");
+        //   assert comp != null;
+        TextField tfContactEmail = new TextField("", "Ex: jobhub@business.tn");
+        TextField tfWebsite = new TextField("", "Ex: www.jobhub.tn");
+        Picker date = new Picker();
+        TextField tfCategory = new TextField("", "Ex: Software Developpment company");
+        TextField tfDescription = new TextField("", "Ex: ");
+        TextField tfCompanyAdress = new TextField("", "Ex: Tunisia, Gafsa");
+        TextField tfContactPhone = new TextField("", "Ex: +21620202020");
+        TextField tfFacebookLink = new TextField("", "Ex: www.facebook.com/jobhub");
+        Button companyButton = new Button("Update your Company");
+        companyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                try {
+                    company comp = new company(0, Session.ConnectedUser.getId(), 1, Integer.parseInt(tfContactPhone.getText()), tfWebsite.getText(), tfCategory.getText(),
+                            tfDescription.getText(), tfCompanyName.getText(), tfContactEmail.getText(), tfCompanyAdress.getText(), tfFacebookLink.getText(), date.getDate());
+                    if (addEditCompanyService.getInstance().addEditCompany(comp)) {
+                        Dialog.show("Success", "Added Successfully !", new Command("OK"));
+                    } else
+                        Dialog.show("ERROR", "Server error", new Command("OK"));
+                } catch (NumberFormatException e) {
+                    Dialog.show("ERROR", "Status must be a number", new Command("OK"));
                 }
-            });
-            addAll(tfCompanyAdress, tfContactEmail, tfWebsite, date, tfCategory, tfDescription, tfCompanyAdress
-                    , tfContactPhone, tfFacebookLink);
-            getToolbar().addMaterialCommandToLeftBar("", FontImage.MATERIAL_ARROW_BACK
-                    , e -> previous.showBack());
-        } else {
-            //  resume.getUser_id() != Session.ConnectedUser.getId()
-            TextField tfCompanyName = new TextField("", comp.getCompanyName());
-            //  assert comp != null;
-            TextField tfContactEmail = new TextField(comp.getContactEmail(), "Ex: jobhub@business.tn");
-            TextField tfWebsite = new TextField(comp.getWebsite(), "Ex: www.jobhub.tn");
-            Picker date = new Picker();
-            TextField tfCategory = new TextField(comp.getCategory(), "Ex: Software Developpment company");
-            TextField tfDescription = new TextField(comp.getDescription(), "Ex: ");
-            TextField tfCompanyAdress = new TextField(comp.getCompanyAddress(), "Ex: Tunisia, Gafsa");
-            TextField tfContactPhone = new TextField(String.valueOf(comp.getContactPhone()), "Ex: +21620202020");
-            TextField tfFacebookLink = new TextField(comp.getFacebookLink(), "Ex: www.facebook.com/jobhub");
-            Button btnValider = new Button("Update Company");
-            btnValider.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent evt) {
-                    try {
-                        company comp = new company(0, Session.ConnectedUser.getId(), 1, Integer.parseInt(tfContactPhone.getText()), tfWebsite.getText(), tfCategory.getText(),
-                                tfDescription.getText(), tfCompanyName.getText(), tfContactEmail.getText(), tfCompanyAdress.getText(), tfFacebookLink.getText(), date.getDate());
-                        if (addEditCompanyService.getInstance().addEditCompany(comp)) {
-                            Dialog.show("Success", "Updated Successfully !", new Command("OK"));
-                        } else
-                            Dialog.show("ERROR", "Server error", new Command("OK"));
-                    } catch (NumberFormatException e) {
-                        Dialog.show("ERROR", "Status must be a number", new Command("OK"));
-                    }
-                }
-            });
-            addAll(tfCompanyName, tfCompanyAdress, tfCategory, tfContactPhone, tfFacebookLink, date, tfWebsite, tfDescription, date, btnValider);
-            getToolbar().addMaterialCommandToLeftBar("", FontImage.MATERIAL_ARROW_BACK
-                    , e -> previous.showBack());
-        }
+            }
+        });
+        addAll(tfCompanyName,tfCompanyAdress,tfCategory,tfDescription,tfWebsite,tfContactEmail,tfFacebookLink,tfContactPhone,companyButton);
+        getToolbar().addMaterialCommandToLeftBar("", FontImage.MATERIAL_ARROW_BACK
+                , e -> previous.showBack());
+
     }
 }
 
--- src/Mobile_App/Gui/User/addEditResume.java
+++ src/Mobile_App/Gui/User/addEditResume.java
@@ -1,4 +1,5 @@
 package Mobile_App.Gui.User;
+
 import Mobile_App.Entities.candidateResume;
 import Mobile_App.Gui.SideMenu;
 import Mobile_App.Service.addEditResumeService;
@@ -11,59 +12,32 @@ import com.codename1.ui.util.Resources;
 
 public class addEditResume extends SideMenu {
     public addEditResume(Form previous, candidateResume resume, Resources res) {
-        setTitle("Add your Resume");
+        setTitle("Update your Resume");
         setLayout(BoxLayout.y());
-        if (resume == null) {
-          //  resume.getUser_id() != Session.ConnectedUser.getId()
-            TextField tfResumeHeadline = new TextField("", "Ex: Web Developper");
-            TextField tfSkills = new TextField("", "Ex: php, symfony, js");
-            TextField tfExperience = new TextField("", "Ex: worked 3years @ Facebook");
-            Button resumeButton = new Button("Add Resume");
-            resumeButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent evt) {
-                    try {
-                        candidateResume resume1 = new candidateResume(0,Session.ConnectedUser.getId(), tfResumeHeadline.getText(),tfSkills.getText(),
-                                tfExperience.getText());
-                        if (addEditResumeService.getInstance().addResume(resume1)) {
-                            Dialog.show("Success", "Added Successfully !", new Command("OK"));
-                        } else
-                            Dialog.show("ERROR", "Server error", new Command("OK"));
-                    } catch (NumberFormatException e) {
-                        Dialog.show("ERROR", "Status must be a number", new Command("OK"));
-                    }
+
+        //  resume.getUser_id() != Session.ConnectedUser.getId()
+        TextField tfResumeHeadline = new TextField("", "Ex: Web Developper");
+        TextField tfSkills = new TextField("", "Ex: php, symfony, js");
+        TextField tfExperience = new TextField("", "Ex: worked 3years @ Facebook");
+        Button resumeButton = new Button("Add Resume");
+        resumeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                try {
+                    candidateResume resume1 = new candidateResume(0, Session.ConnectedUser.getId(), tfResumeHeadline.getText(), tfSkills.getText(),
+                            tfExperience.getText());
+                    if (addEditResumeService.getInstance().addResume(resume1)) {
+                        Dialog.show("Success", "Added Successfully !", new Command("OK"));
+                    } else
+                        Dialog.show("ERROR", "Server error", new Command("OK"));
+                } catch (NumberFormatException e) {
+                    Dialog.show("ERROR", "Status must be a number", new Command("OK"));
                 }
-            });
-            addAll(tfResumeHeadline,tfSkills,tfExperience,resumeButton);
-            getToolbar().addMaterialCommandToLeftBar("", FontImage.MATERIAL_ARROW_BACK
-                    , e -> previous.showBack());
-        } else {
-            TextField tfResumeHeadline = new TextField(resume.getResumeHeadline(), "Ex: Web Developper");
-            TextField tfSkills = new TextField("", "Ex: php, symfony, js");
-            TextField tfExperience = new TextField("", "Ex: worked 3years @ Facebook");
-
-            Button btnValider = new Button("Update Resume");
-
-            btnValider.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent evt) {
-                    try {
-                        candidateResume resume1 = new candidateResume(resume.getId(),Session.ConnectedUser.getId(), tfResumeHeadline.getText(),tfSkills.getText(),
-                                tfExperience.getText());
-                        if (addEditResumeService.getInstance().addResume(resume1)) {
-                            Dialog.show("Success", "Updated Successfully !", new Command("OK"));
-                        } else
-                            Dialog.show("ERROR", "Server error", new Command("OK"));
-                    } catch (NumberFormatException e) {
-                        Dialog.show("ERROR", "Status must be a number", new Command("OK"));
-                    }
-                }
-            });
-
-            addAll(tfResumeHeadline,tfSkills,tfExperience,btnValider);
-            getToolbar().addMaterialCommandToLeftBar("", FontImage.MATERIAL_ARROW_BACK
-                    , e -> previous.showBack());
-        }
+            }
+        });
+        addAll(tfResumeHeadline, tfSkills, tfExperience, resumeButton);
+        getToolbar().addMaterialCommandToLeftBar("", FontImage.MATERIAL_ARROW_BACK
+                , e -> previous.showBack());
     }
-    }
+}
 
